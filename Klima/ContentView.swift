84156102d8f5501ffd41cff4cde6d24f7b27dcf0
MarcLennard <marclennard.vlaminck@gmail.com>
2020-09-25 11:55:01 +0200

    Initial Commit

---
+++ Klima/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Klima
+//
+//  Created by Marc Lennard Vlaminck on 25.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Klima/KlimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlimaApp.swift
+//  Klima
+//
+//  Created by Marc Lennard Vlaminck on 25.09.20.
+//
+
+import SwiftUI
+
+@main
+struct KlimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
